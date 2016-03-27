3491943a3b3ab2e274e409076906c1be1181fb2f
Oscar Guzman Giraldo <Oscar Guzman Giraldo>
2016-03-27 12:30:10 -0500

    cropit y algo de cargador de imagenes

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,32 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $(".button-collapse").sideNav();
-	$(".dropdown-button").dropdown();
+	 $(".dropdown-button").dropdown();
         
 
     //colapsable acodion para sidenav 
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-
-    
+    });    
   });
+
+  $(function() {
+        $('.image-editor').cropit({
+          imageState: {
+            src: 'http://lorempixel.com/500/400/',
+          },
+        });
+
+        $('.rotate-cw').click(function() {
+          $('.image-editor').cropit('rotateCW');
+        });
+        $('.rotate-ccw').click(function() {
+          $('.image-editor').cropit('rotateCCW');
+        });
+
+        $('.export').click(function() {
+          var imageData = $('.image-editor').cropit('export');
+          window.open(imageData);
+        });
+      });
           
